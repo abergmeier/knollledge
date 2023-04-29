f95ff23b3f825c2598b4a261c533994a51a05cc9
Andreas Bergmeier <abergmeier@gmx.net>
2023-04-29 21:10:14 +0200

    Define predefined Code Searches

---
+++ internal/config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import "github.com/abergmeier/knollledge/internal/job"
+
+var (
+	predefinedCodeSearches = map[string]job.MakeCodeSearchFunc{
+		"bazel-package":           job.MakeBazelPackageCodeSearch,
+		"buf-configuration":       job.MakeBufConfigurationCodeSearch,
+		"cargo-configuration":     job.MakeCargoConfigurationCodeSearch,
+		"container-configuration": job.MakeContainerConfigurationCodeSearch,
+		"go-modules":              job.MakeGoModuleCodeSearch,
+		"poetry-configuration":    job.MakePoetryConfigurationCodeSearch,
+		"protobuf-definition":     job.MakeProtobufDefinitionCodeSearch,
+		"skaffold-configuration":  job.MakeSkaffoldConfigurationCodeSearch,
+		"terraform-backend":       job.MakeTerraformBackendCodeSearch,
+		"terraform-gke-cluster":   job.MakeTerraformGKECluster,
+	}
+)
+
+func mustToCodeSearch(name string, queryPrefix string) job.CodeSearch {
+	csf := predefinedCodeSearches[name]
+	return csf(queryPrefix)
+}
--- internal/job/codesearch.go
+++ internal/job/codesearch.go
@@ -46,3 +46,64 @@ func (cs CodeSearch) mustRunWithClient(ctx context.Context, c github.Client) *gi
 	}
 	return csr
 }
+
+type MakeCodeSearchFunc func(queryPrefix string) CodeSearch
+
+var (
+	_ MakeCodeSearchFunc = MakeBazelPackageCodeSearch
+	_ MakeCodeSearchFunc = MakeBufConfigurationCodeSearch
+	_ MakeCodeSearchFunc = MakeCargoConfigurationCodeSearch
+	_ MakeCodeSearchFunc = MakeContainerConfigurationCodeSearch
+	_ MakeCodeSearchFunc = MakeGoModuleCodeSearch
+	_ MakeCodeSearchFunc = MakePoetryConfigurationCodeSearch
+	_ MakeCodeSearchFunc = MakeProtobufDefinitionCodeSearch
+	_ MakeCodeSearchFunc = MakeSkaffoldConfigurationCodeSearch
+	_ MakeCodeSearchFunc = MakeTerraformBackendCodeSearch
+	_ MakeCodeSearchFunc = MakeTerraformGKECluster
+)
+
+func MakeBazelPackageCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:**/BUILD OR path:**/BUILD.bazel")
+}
+
+func MakeBufConfigurationCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:**/buf.yaml")
+}
+
+func MakeCargoConfigurationCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:**/Cargo.toml")
+}
+
+func MakeContainerConfigurationCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:**/Containerfile OR path:**/Dockerfile FROM")
+}
+
+func MakeGoModuleCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:**/go.mod")
+}
+
+func MakePoetryConfigurationCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:**/poetry.lock")
+}
+
+func MakeProtobufDefinitionCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:*.proto")
+}
+
+func MakeSkaffoldConfigurationCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:**/skaffold.yaml")
+}
+
+func MakeTerraformBackendCodeSearch(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, "path:*.tf backend")
+}
+
+func MakeTerraformGKECluster(queryPrefix string) CodeSearch {
+	return makePrefixedCodeSearch(queryPrefix, `path:*.tf "google_container_cluster"`)
+}
+
+func makePrefixedCodeSearch(queryPrefix, query string) CodeSearch {
+	return CodeSearch{
+		Query: fmt.Sprint(queryPrefix, query),
+	}
+}
